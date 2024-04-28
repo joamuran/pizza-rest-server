a3e2b97b96e68f1505341b226768219d9173bfaf
José A. Múrcia <joamuran@gmail.com>
2024-04-28 08:11:55 +0200

    Fixed -1 as pageNumber and size to get all pizza

--- models/pizzaModel.js
+++ models/pizzaModel.js
@@ -2,8 +2,17 @@ import { pizzesData } from "./pizzaData.js";
 
 // Funció per obtenir les pizzes amb paginació
 export function getPizzes(pageNumber, pageSize) {
-    const startIndex = (pageNumber - 1) * pageSize;
-    const endIndex = startIndex + pageSize;
+
+    let startIndex = (pageNumber - 1) * pageSize;
+    let endIndex = startIndex + pageSize;
+
+    if(pageNumber ==-1 || pageSize==-1){
+        // Si qualsevol dels dos és -1, 
+        // Tornem totes les pizzes
+        startIndex=0;
+        endIndex=pizzesData.length;
+    }
+
     const pizzes = pizzesData.slice(startIndex, endIndex);
 
     return {
