8e88be32e6cebfa1adea8c65329f84cc175776be
José A. Múrcia <joamuran@gmail.com>
2024-04-29 13:44:23 +0200

    Fixed entrants and images

--- models/entrantsData.js
+++ models/entrantsData.js
@@ -2,21 +2,25 @@ export const entrantsData = [
     {
         id: "nuggetsx6",
         nom: 'Nuggets, 6 unitats',
+        img: '/img/nuggets.png',
         preu: 1.50,
     },
     {
         id: "nuggetsx9",
         nom: 'Nuggets, 9 unitats',
+        img: '/img/nuggets.png',
         preu: 2.0,
     },
     {
         id: "arosx6",
         nom: 'Aros de ceba, 6 unitata',
+        img: '/img/aros.png',
         preu: 1.50,
     },
     {
         id: "arosx9",
         nom: 'Aros de ceba, 9 unitata',
+        img: '/img/aros.png',
         preu: 2.0,
     },
 ];
