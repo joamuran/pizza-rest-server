d4e05585c498aadbfb55763b4dc11ef5952f676b
Jose A. Murcia <joamuran@gmail.com>
2025-03-03 18:53:59 +0100

    Added details

--- models/begudesData.js
+++ models/begudesData.js
@@ -3,16 +3,33 @@ export const begudaData = [
         id: "aigua",
         nom: 'Aigua 33 cl',
         preu: 1.50,
+        sucre: false,
+        cafeina: false,
+        alcohol: false
     },
     {
         id: "cola",
         nom: 'Coca Cola 33 cl',
         preu: 1.60,
+        sucre: true,
+        cafeina: true,
+        alcohol: false
     },
     {
         id: "cola0",
         nom: 'Coca Cola Zero 33 cl',
         preu: 1.60,
+        sucre: false,
+        cafeina: true,
+        alcohol: false
+    },
+    {
+        id: "cervessa",
+        nom: 'Mahou 33 cl',
+        preu: 1.60,
+        sucre: false,
+        cafeina: false,
+        alcohol: true
     },
 ];
 
